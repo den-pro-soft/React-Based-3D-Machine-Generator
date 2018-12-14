35bca1a7321492b8073a9617204c3b2ff03df75c
VictorB <VictorB@jdev.com.ua>
2018-12-14 12:49:25 +0200

    add npm to project (reorgonizatoin of code)

---
+++ README.md
@@ -0,0 +1,8 @@
+###About
+The frontend part of eMachineShop CAD project.
+
+###Compile
+For compiling 3DView.js need using <code>npm start</code> or <code>npm compile</code> for compiling, it can be use on deployment on a production server.
+
+Before run the command execute <code>npm install</code> for installing all dependency.
+
---
+++ js/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development", // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+    entry: "./src/3Dview.js",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "3Dview.js",
+        publicPath: "assets/", // string    // the url to the output directory resolved relative to the HTML page
+        library: "3DView",
+        libraryTarget: "umd", // universal module definition    // the type of the exported library
+        /* Advanced output configuration (click to show) */
+    },
+    module: {
+        // configuration regarding modules
+        rules: [
+            // rules for modules (configure loaders, parser options, etc.)
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, "app")
+                ],
+                exclude: [
+                    path.resolve(__dirname, "app/demo-files")
+                ],
+                // conditions for the issuer (the origin of the import)
+                enforce: "pre",
+                enforce: "post",
+                // flags to apply these rules, even if they are overridden (advanced option)
+                loader: "babel-loader",
+                // the loader which should be applied, it'll be resolved relative to the context
+                // -loader suffix is no longer optional in webpack2 for clarity reasons
+                // see webpack 1 upgrade guide
+                options: {
+                    presets: ["es2015"]
+                },
+                // options for the loader
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    // apply multiple loaders and options
+                    "htmllint-loader",
+                    {
+                        loader: "html-loader",
+                        options: {
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+}
