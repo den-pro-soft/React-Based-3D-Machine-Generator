5c44ac2abb643a5445dc066f5f9fc9efcf5829c2
VictorB <VictorB@jdev.com.ua>
2019-01-10 19:53:51 +0200

    rulex

--- js/src/2d/Board.js
+++ js/src/2d/Board.js
@@ -5,7 +5,6 @@
 import LineTool from './tool/LineTool';
 import Document from './../model/Document';
 import Point from './../model/Point';
-import Line from './../model/Line';
 
 export default class Board {
     /**
@@ -68,7 +67,7 @@ export default class Board {
     }
 
     mouseMove(e) {
-        if (!this.tool.mouseMove()) { //todo: create point for tool
+        if (!this.tool.mouseMove(this._convertToGlobalCoordinateSystem({x:e.offsetX, y:e.offsetY}))) {
             if (this._mouseDown) {
                 this.setBias(this._bias.x - (this._mouseDown.offsetX - e.offsetX)
                             ,this._bias.y - (this._mouseDown.offsetY - e.offsetY));
@@ -95,7 +94,7 @@ export default class Board {
     _mouseWheel(e) {
         let dScale = e.deltaY / 500;
         let was = this._convertToGlobalCoordinateSystem({x:e.offsetX, y:e.offsetY});
-        if(this.setScale(this._scale - dScale)) {
+        if(this.setScale(this._scale*(1+dScale))) {
             let now = this._convertToGlobalCoordinateSystem({x:e.offsetX, y:e.offsetY});
             this.setBias(this._bias.x+((now.x-was.x)*this._pixelPerOne*this._scale)
                         ,this._bias.y-((now.y-was.y)*this._pixelPerOne*this._scale));
@@ -111,7 +110,7 @@ export default class Board {
     }
 
     setScale(scale){
-        if(scale>10 || scale <0.1){
+        if(scale>1000 || scale <0.001){
             return false;
         }
         this._scale=scale;
@@ -119,18 +118,114 @@ export default class Board {
     }
 
     _drawRulers() {
+
+        this.drawLine({x: 0, y: 0}, {x: 10, y: 0}, '#00ff00', 1);
+
+
         let rulerWidth = 20;
         let rulerBackgroundColor = '#efefef';
         let fillColor = '#000000';
 
+        this._context.font = "400 9px Arial";
+        this._context.textBaseline = "middle";
+        this._context.textAlign = "center";
+        this._context.fillStyle = 'rgba(50, 50, 50, 1.0)';
+        this._context.strokeStyle = 'rgba(125, 125, 125, 0.5)';
+
+
+        console.log(this._scale);
+
         this._drawRect({x: rulerWidth, y: 0}, {x: this._width, y: rulerWidth}, rulerBackgroundColor, true);
+        let convertX =x=>x*this._pixelPerOne*this._scale+this._initCenterPosition.x+this._bias.x;
+        for (let x = 0; x < 100/this._scale; x++) {
+            let localX = convertX(x);
+            let l = 1;
+            l = x%(this._scale<1?10:l)==0? 10:5;
+            l = x%(this._scale<=0.1?20:l)==0? 10:5;
+            l = x%(this._scale<=0.05?50:l)==0? 10:5;
 
-
-        for (let x = 0; x < 10; x ++) { //if x<0
-            this.drawLine({x: x, y: 0}, {x: x, y: -1}, fillColor, 1);
+            if(l==10){
+                this._context.fillText(x, localX, 6);
+            }
+            this._context.fillRect(localX, rulerWidth-l, 1, l);
         }
-        this.drawLine({x: 0, y: 0}, {x: 10, y: 0}, fillColor, 1);
+        for (let x = 0; x>-100; x--) {
+            let localX = convertX(x);
+            let l = x%(this._scale>1?1:10)==0? 10:5;
 
+
+            if(x%10==0){
+                this._context.fillText(x, localX, 6);
+            }
+            this._context.fillRect(localX, rulerWidth-l, 1, l);
+        }
+
+
+
+
+
+
+
+
+
+        //********************************** coordinates *********************
+        var len = 50 * this._scale;
+
+        len = Math.round(len / 50 + 1) * 50;
+
+        var step = len * this._scale;
+        if (step < 30) {len = (len * 2 / 100 + 1) * 100;
+            if (len > 1000) len = (len * 2 / 1000 + 1) * 1000; if (len > 3000)
+                len = (len * 2 / 1000 + 1) * 1000; step = len * this._scale}
+
+        for (var n = 0; n < 5; n++){
+            if (step > 80) if (len == 5) {len = 2; step = len * this._scale}
+            if (step > 80) if (len == 10) {len = 5; step = len * this._scale}
+            if (step > 80) if (len == 15) {len = 10; step = len * this._scale}
+            if (step > 80) if (len == 25) {len = 10; step = len * this._scale}
+            if (step > 80) if (len == 50) {len = 20; step = len * this._scale}
+            if (step > 80) if (len == 100) {len = 50; step = len * this._scale}
+            if (step > 80) if (len == 150) {len = 100; step = len * this._scale}
+            if (step > 80) if (len == 200) {len = 100; step = len * this._scale}
+            if (step > 80) if (len == 250) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 300) {len = 150; step = len * this._scale}
+            if (step > 80) if (len == 350) {len = 250; step = len * this._scale}
+            if (step > 80) if (len == 400) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 450) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 500) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 550) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 600) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 650) {len = 200; step = len * this._scale}
+            if (step > 80) if (len == 700) {len = 500; step = len * this._scale}
+            if (step > 80) if (len == 750) {len = 500; step = len * this._scale}
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        this._drawRect({x: 0, y: 0}, {x: rulerWidth, y: this._height}, rulerBackgroundColor, true);
+        let convertY =y=>y*this._pixelPerOne*this._scale+this._initCenterPosition.y+this._bias.y;
+        for (let y = 0; y < 100; y++) {
+            this._drawLine({x:rulerWidth-5, y: convertY(y)}, {x: rulerWidth, y: convertY(y)}, fillColor, 1);
+        }
+        for (let y = 0; y>-100; y--) {
+            this._drawLine({x:rulerWidth-5, y: convertY(y)}, {x: rulerWidth, y: convertY(y)}, fillColor, 1);
+        }
+
+
+        this._drawRect({x: 0, y: 0}, {x: rulerWidth+5, y: rulerWidth+5}, rulerBackgroundColor, true);
     }
 
     /**
