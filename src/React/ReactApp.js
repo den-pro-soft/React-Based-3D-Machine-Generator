eed0a89e5b6a46c88923ed9e0bf4de5b10beaff9
OlegGre4ka <gre4kae@ukr.net>
2019-01-15 19:18:21 +0300

    #8736-Created components for modal windows Aluminum 6061 and Select a Finish

--- src/React/ReactApp.js
+++ src/React/ReactApp.js
@@ -1,15 +1,29 @@
 import React from "react";
+import './react-app.scss';
 import LeftMenu from "./LeftMenu/LeftMenu";
 import UpMenu from "./UpMenu/UpMenu";
+import Aluminium6061 from "./RightButtons/Aluminium6061";
+import SelectFinish from "./RightButtons/SelectFinish";
 
 const ReactApp = context => {
   return (
-    <>
-      <div>
-        <UpMenu />
-        <LeftMenu />
+    <div className="ReactApp">
+      <div
+        className="Up-End-RightMenu"
+        // style={{ display: "inline-flex", justifyContent: "space-between"}}
+      >
+        <>
+          <UpMenu />
+        </>
+        <div className="RightButtons" 
+        // style={{ display: "inline-flex", justifyContent: "space-between" }}       
+        >
+          <Aluminium6061 />
+          <SelectFinish />
+        </div>
       </div>
-    </>
+      <LeftMenu />
+    </div>
   );
 };
 
