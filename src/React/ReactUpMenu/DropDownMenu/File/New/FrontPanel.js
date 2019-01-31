a12d834dd81245801d8d1e1e216766294ecb5354
OlegGre4ka <gre4kae@ukr.net>
2019-01-31 14:20:03 +0300

    File-New-FrontPanel

--- src/React/ReactUpMenu/DropDownMenu/File/New/FrontPanel.js
+++ src/React/ReactUpMenu/DropDownMenu/File/New/FrontPanel.js
@@ -1,16 +1,67 @@
 import React from "react";
 import "./front-panel.scss";
+import Button from "@material-ui/core/Button";
 
 export default class FrontPanel extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-        value:6000
-    }
+    this.state = {
+      lengthValue: `6,000${String.fromCharCode(34)}`,
+      width: `2,000${String.fromCharCode(34)}`,
+      diameter: `0,150${String.fromCharCode(34)}`
+    };
   }
-onChangeInput =event => {
+  handleInputLenght = event => {
+    event.preventDefault();
 
+    this.setState({ lengthValue: event.target.value  });
+    // if(event.charCode == '13'){
+
+    //     this.setState({
+    //       lengthValue: this.state.lengthValue + `${String.fromCharCode(34)}`
+    //     });
+    // }
+    console.log(
+      this.state.lengthValue,
+      event.target.value,
+      "this.state.lengthValue"
+    );
+  };
+  handleClickInputLenght = event => {
+    event.preventDefault();
+if(event.charCode == '13'){
+
+    this.setState({
+      lengthValue: this.state.lengthValue + `${String.fromCharCode(34)}`
+    });
 }
+    console.log(
+      event.charCode,
+      event.target.value,
+      "charCode===13"
+    );
+  };
+
+  handleInputWidth = event => {
+    event.preventDefault();
+
+    this.setState({ width: event.target.value });
+    console.log(this.state.width, "this.state.width");
+  };
+
+  handleInputDiameter = event => {
+    event.preventDefault();
+
+    this.setState({ diameter: event.target.value });
+    console.log(this.state.diameter, "this.state.diameter");
+  };
+  resetButton = () => {
+    this.setState({
+      lengthValue: `6,000${String.fromCharCode(34)}`,
+      width: `2,000${String.fromCharCode(34)}`,
+      diameter: `0,150${String.fromCharCode(34)}`
+    });
+  };
   render() {
     return (
       <div className="FrontPanel">
@@ -18,9 +69,9 @@ onChangeInput =event => {
           A polygon where each side has the same lenght, and all interior angles
           are equal and less then 180 degrees.
         </p>
-        <p className="Parameters">Parameters:</p>
+        {/* <p className="Parameters">Parameters:</p> */}
 
-        {/* <div className="Parameters">
+        <div className="Parameters">
           <p>Parameters:</p>
           <Button
             onClick={this.resetButton}
@@ -34,7 +85,8 @@ onChangeInput =event => {
           >
             Reset
           </Button>
-        </div> */}
+        </div>
+
         <table>
           <thead>
             <tr>
@@ -46,16 +98,46 @@ onChangeInput =event => {
             <tr>
               <td>Length</td>
               <td className="Value">
-                <input type="text" value={this.state.value+`${String.fromCharCode(34)}`} />
+                <input
+                  type="text"
+                  className="Input"
+                  value={
+                    this.state.lengthValue /*`${String.fromCharCode(34)}`*/
+                  }
+                  onChange={this.handleInputLenght}
+                //   onKeyUp={this.handleInputLenght}
+
+                  onKeyUp={this.handleClickInputLenght}
+                />
               </td>
             </tr>
             <tr>
               <td>Width</td>
-              <td className="Value">2000</td>
+              {/* <td className="Value">2000</td> */}
+              <td>
+                <input
+                  type="text"
+                  className="Input"
+                  value={this.state.width /*`${String.fromCharCode(34)}`*/}
+                  onChange={this.handleInputWidth}
+                //   onKeyPress={this.handleClickInputLenght}
+
+                />
+              </td>
             </tr>
             <tr>
               <td>Mounting Hole Diameter</td>
-              <td className="Value">0.150{String.fromCharCode(34)}</td>
+              {/* <td className="Value">0.150{String.fromCharCode(34)}</td> */}
+              <td>
+                <input
+                  type="text"
+                  className="Input"
+                  value={this.state.diameter /*`${String.fromCharCode(34)}`*/}
+                  onChange={this.handleInputDiameter}
+                //   onKeyPress={this.handleClickInputLenght}
+
+                />
+              </td>
             </tr>
           </tbody>
         </table>
