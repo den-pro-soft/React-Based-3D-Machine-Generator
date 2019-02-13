e14e5dbf87e70dd008080a7f06368e5fc507e57d
OlegGre4ka <gre4kae@ukr.net>
2019-02-13 16:30:50 +0300

    Circle in Region

--- src/file/FileLoader.js
+++ src/file/FileLoader.js
@@ -22,15 +22,24 @@ export default class FileLoader {
       }
       
       if (el.typeName === "Arc") {
-        if(el.startAngle===0 && el.endAngle===0){
-          return `<Circle Center="${el._center.x},${el._center.y}" Radius="${
-            el.radius
-          }"/>`;
+        if (el.startAngle === 0 && el.endAngle === 0) {
+          //  `<Circle Center="${el._center.x},${el._center.y}" Radius="${
+          //   el.radius
+          //   }"/>`;
+          return `<Region BaseHeight="0" Z="${el.height}" BottomType="Drill" ThroughHole="">
+    <Machine Id="41" Name="Auto"/>
+        <Contour>
+            <Circle Center="${el._center.x},${el._center.y}" Radius="${el.radius}"/>
+        </Contour>
+</Region>`;
         } else {
-        return `<Arc Center="${el._center.x},${el._center.y}" Radius="${
-          el.radius
-        }" StartAngle="${el.startAngle}" IncAngle="${el.endAngle}"/>`;
-      }
+        
+            let IncAngle =(el.endAngle > el.startAngle)? (el.endAngle - el.startAngle) : (el.startAngle + (360 - el.endAngle))
+
+          return `<Arc Center="${el._center.x},${el._center.y}" Radius="${
+            el.radius
+            }" StartAngle="${el.startAngle}" IncAngle="${IncAngle}"/>`;
+        }
       }
   
       if (el.typeName === "Spline") {
@@ -41,42 +50,35 @@ export default class FileLoader {
         },${el._points[1].y}"/> `;
       }
 
-    });
-
-    const comment = elements.map(el => {
       if (el.lineType.name!=="Auto"&&el.typeName === "Line") {
          
-       return  `<Region BaseHeight="0" Z="${el.height}">
-          <Machine Id="14" Name="Comment" CTM="0"/>
-            <Contour>
-              <Straight P1="${el.p1.x},${el.p1.y}" P2="${el.p2.x},${el.p2.y}"/>
-            </Contour>
-        </Region>`
-      }
-    // })
-    // const text = elements.map(el => {
-      if (el.typeName === "Text") {
-         
-       return  `<Region BaseHeight="0" Z="${el.height}">
-          <Machine Id="14" Name="Comment" CTM="0"/>
-            <Contour>
-              <Text Position="${el.position.x},${el.position.y}" Height="${el.fontSize}" FontName="" HFlip="0" VFlip="0" Angle="${el.angle}">${el.text}</Text>      
-            </Contour>
-        </Region>`
-      }
-    })
+        return  `<Region BaseHeight="0" Z="${el.height}">
+     <Machine Id="14" Name="Comment" CTM="0"/>
+         <Contour>
+               <Straight P1="${el.p1.x},${el.p1.y}" P2="${el.p2.x},${el.p2.y}"/>
+         </Contour>
+ </Region>`
+       }
+     
+       if (el.typeName === "Text") {
+          
+        return  `<Region BaseHeight="0" Z="${el.height}">
+     <Machine Id="14" Name="Comment" CTM="0"/>
+         <Contour>
+             <Text Position="${el.position.x},${el.position.y}" Height="${el.fontSize}" FontName="" HFlip="0" VFlip="0" Angle="${el.angle}">${el.text}</Text>      
+         </Contour>
+ </Region>`
+       }
+    });
+
+
     const header =
       '<?xml version="1.0"?>\n' +
       '<eMachineShop3DObjects VersionId="1.1">\n' +
       '<View Type="Top">\n';
-    const Contour =
-      "<Region>\n" +
-      "<Machine/>\n" +
-      "<Contour>\n" +
-      Figures.join("\n") +
-      "</Contour>\n" +
-      "</Region>\n"+
-      comment.join("\n");
+      
+    const Contour = Figures.join("\n") +'\n';
+   
     const footer =
       "</View>\n" +
       '<QuantityOfParts Value="10"/>\n' +
