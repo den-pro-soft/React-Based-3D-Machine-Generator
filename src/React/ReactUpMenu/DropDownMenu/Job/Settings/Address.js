f314790840ca201ed29a4703a0f18f4164717a1e
OlegGre4ka <gre4kae@ukr.net>
2019-02-21 09:33:25 +0300

    routing and redux working,Summary saving data

--- src/React/ReactUpMenu/DropDownMenu/Job/Settings/Address.js
+++ src/React/ReactUpMenu/DropDownMenu/Job/Settings/Address.js
@@ -3,7 +3,7 @@ import "./addres.scss";
 import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import { connect } from "react-redux";
-class Adress extends React.Component {
+export default class Adress extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,36 +13,69 @@ class Adress extends React.Component {
       provinceOther: false,
       provinceOther2: false,
       value: "U.S.A.",
-      // firstName:'',
-      // lastName:'',
+      firstName:'',
+      lastName:'',
       businessName:'',
       email:''
     };
   }
+  componentWillMount(){
+    const FirstName = localStorage.getItem('firstName');
+    if(FirstName===null){
+    this.setState({firstName:this.state.firstName})
+    } else{
+      this.setState({firstName:FirstName})
 
-  // handleFirstNameChange = (e) =>{
-  //   this.setState({firstName: e.target.value},()=>{
-  //   this.props.updateFirstName(this.state.firstName)
-  //   });
-  // }
+    }
 
-  // handleLastNameChange = (e) =>{
-  //   // this.props.handleLastNameChange(e.target.value)
-  //   this.setState({lastName: e.target.value},()=>{
-  //     console.log(this.state.lastName,'lastName')
-  //   this.props.updateLastName(this.state.lastName)
-  //   });
-  // }
+    const LastName = localStorage.getItem('lastName');
+    if(LastName===null){
+    this.setState({lastName:this.state.lastName})
+    } else{
+      this.setState({lastName:LastName})
+
+    }
+
+    const BusinessName = localStorage.getItem('businessName');
+    if(BusinessName===null){
+    this.setState({businessName:this.state.businessName})
+    } else{
+      this.setState({businessName:BusinessName})
+
+    }
+  
+    const Email = localStorage.getItem('email');
+    if(Email ===null){
+    this.setState({email:this.state.email})
+    } else{
+      this.setState({email:Email })
+    }
+  }
+  handleFirstNameChange = (e) =>{
+    this.setState({firstName: e.target.value},()=>{
+    localStorage.setItem('firstName', this.state.firstName);
+
+    });
+  }
+
+  handleLastNameChange = (e) =>{
+    this.setState({lastName: e.target.value},()=>{
+      console.log(this.state.lastName,'lastName')
+    localStorage.setItem('lastName', this.state.lastName);
+
+    });
+  }
 
   handleBusinessNameChange = (e) =>{
     this.setState({businessName: e.target.value},()=>{
-    this.props.updateBusinessName(this.state.businessName)
+    localStorage.setItem('businessName', this.state.businessName);
     });
   }
 
   handleEmailChange = (e) =>{
     this.setState({email: e.target.value},()=>{
-    this.props.updateEmail(this.state.email)
+      localStorage.setItem('email', this.state.email);
+      // this.props.updateEmail(this.state.email)
     });
   }
 
@@ -211,24 +244,25 @@ class Adress extends React.Component {
               <div className="InputGroup">
                 <div className="Input">
                   <input type="text" 
-                   value={this.props.firstName}
-                  //  onChange={this.handleFirstNameChange}
-                  onChange = {(e) => {
-                    this.props.handleFirstNameChange(
-                      e.target.value
-                    );
-                  }} />
+                   value={this.state.firstName}
+                   onChange={this.handleFirstNameChange}
+                  // onChange = {(e) => {
+                  //   this.props.handleFirstNameChange(
+                  //     e.target.value
+                  //   );
+                  // }} 
+                  />
                 </div>
                 <div className="Input">
                   <input type="text" 
                     value={this.state.lastName}
-                    // value={this.props.lastName}
-                    // onChange={this.handleLastNameChange}
-                    onChange = {(e) => {
-                      this.props.handleLastNameChange(
-                        e.target.value===undefined?e.target.value='':e.target.value
-                      );
-                    }} 
+                    value={this.state.lastName}
+                    onChange={this.handleLastNameChange}
+                    // onChange = {(e) => {
+                    //   this.props.handleLastNameChange(
+                    //     e.target.value===undefined?e.target.value='':e.target.value
+                    //   );
+                    // }} 
                     />
                 </div>
                 <div className="Input">
@@ -468,22 +502,3 @@ class Adress extends React.Component {
     );
   }
 }
-const mapStateToProps = state => {
-  return {
-    firstName: state.summaryReducer.firstName,
-    lastName: state.summaryReducer.lastName
-
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    handleFirstNameChange: value => {
-      dispatch({ type: "FIRST_NAME", payload: value });
-    },
-    handleLastNameChange: value => {
-      dispatch({ type: "LAST_NAME", payload1: value });
-    }
-   } 
-};
-export default connect(mapStateToProps,mapDispatchToProps)(Adress);
