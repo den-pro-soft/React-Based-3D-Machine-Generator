1cdf811c29ff36d38d1344d5a607f597916a5420
OlegGre4ka <gre4kae@ukr.net>
2019-03-04 20:52:45 +0300

    First-Last Name in OrderWindow

--- src/React/ReactUpMenu/DropDownMenu/Job/Price/Order/StandingPart.js
+++ src/React/ReactUpMenu/DropDownMenu/Job/Price/Order/StandingPart.js
@@ -10,6 +10,24 @@ export default class StandingPart extends React.Component {
     return (
       <div className="StandingPart">
         <h3 className="Title">Shipping Address</h3>
+        <div className="FirstLastName">
+          <div className="First">
+            <div className="LabelFirst">
+              <label>First Name:</label>
+            </div>
+            <div className="InputText">
+              <input className="InputField" type="text" />
+            </div>
+          </div>
+          <div className="Last">
+            <div className="LabelLast">
+              <label>Last Name:</label>
+            </div>
+            <div className="InputText">
+              <input className="InputField" type="text" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Redux/reducers/priceReducer.js
+++ src/Redux/reducers/priceReducer.js
@@ -9,7 +9,7 @@ export default function priceReducer(state = initialState, action) {
     case "OPEN_PRICE":
       return { openPrice: action.payload,openOrder: !action.payload };
     case "CLOSE_PRICE":
-      return { openPrice: action.payload };
+      return { openPrice: action.payload,openOrder: action.payload };
     case "OPEN_ORDER":
       return { openOrder: action.payload, openPrice: !action.payload };
       //working case
