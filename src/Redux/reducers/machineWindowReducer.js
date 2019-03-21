1e309553fa6d0e11b68ddabe91f5f360686c8883
OlegGre4ka <gre4kae@ukr.net>
2019-03-21 17:42:57 +0300

    modal Machine-Auto-mark-up

--- src/Redux/reducers/machineWindowReducer.js
+++ src/Redux/reducers/machineWindowReducer.js
@@ -1,13 +1,13 @@
 const initialState = {
-  openTapModal: false
+  openMachineModal: false
 };
 
 export default function machineWindowReducer(state = initialState, action) {
   switch (action.type) {
-    case "OPEN_TAP_MODAL":
-      return { openTapModal: action.payload };
-    case "CLOSE_TAP_MODAL":
-      return { openTapModal: action.payload };
+    case "OPEN_MACHINE_MODAL":
+      return { openMachineModal: action.payload };
+    case "CLOSE_MACHINE_MODAL":
+      return { openMachineModal: action.payload };
     default:
       return state;
   }
