c9f73ec1d48dbacb5d1a3b30c557ba09c55a3733
slarda <gianttiny123@gmail.com>
2019-04-01 16:59:53 +0300

    fix ruler arrows render

--- src/ui/2d/renderer/LineRenderer.js
+++ src/ui/2d/renderer/LineRenderer.js
@@ -37,7 +37,7 @@ export default class LineRenderer extends Render{
 
         let center = vector.getCenter();
         vector.rotate(center,90);
-        vector.move(-vector._line.B/2,vector._line.A/2);
+        vector.move(-vector._line.A/2,vector._line.B/2);
 
         let circle = new Arc(this.element.p2.copy(), 1);
         let crossPoints = vector.toPolyLines()[0].getCrossPoints(circle.toPolyLines()[0]);
@@ -46,30 +46,30 @@ export default class LineRenderer extends Render{
             vector.p1 = crossPoints[0];
             vector.p2 = crossPoints[1];
             this.board.drawLine(vector.p1, vector.p2);
-            vector.move(-this.element._line.A,-this.element._line.B);
+            vector.move(-this.element._line.B,-this.element._line.A);
             this.board.drawLine(vector.p1, vector.p2);
             vector.rotate(vector.getCenter(), 65);
-            if(this.element._line.A>0) {
+            if(this.element._line.B>0) {
                 vector.p2 = vector.getCenter();
             }else{
                 vector.p1 = vector.getCenter();
             }
             this.board.drawLine(vector.p1, vector.p2);
-            if(this.element._line.A>0) {
+            if(this.element._line.B>0) {
                 vector.rotate(vector.p2, 50);
             }else{
                 vector.rotate(vector.p1, 50);
             }
             this.board.drawLine(vector.p1, vector.p2);
 
-            vector.move(this.element._line.A,this.element._line.B);
-            if(this.element._line.A>0) {
+            vector.move(this.element._line.B,this.element._line.A);
+            if(this.element._line.B>0) {
                 vector.rotate(vector.p2, 180);
             }else{
                 vector.rotate(vector.p1, 180);
             }
             this.board.drawLine(vector.p1, vector.p2);
-            if(this.element._line.A>0) {
+            if(this.element._line.B>0) {
                 vector.rotate(vector.p2, -50);
             }else{
                 vector.rotate(vector.p1, -50);
