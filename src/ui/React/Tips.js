93db4e0bf74c1f909d7cb20f3fc3fcea34cf8750
slarda <gianttiny123@gmail.com>
2019-04-15 11:22:00 +0300

    TR #180610. CAD - Toolbar button tips - See edits a bottom here....

--- src/ui/React/Tips.js
+++ src/ui/React/Tips.js
@@ -6,45 +6,73 @@ export default class Tips{
     constructor(){
 
         this.tips = [];
-        this.tips['group'] = "Group (Ctrl +G) </br> Combine selected lines into one group to allow selection in </br> " +
-            "one click. Hold the SHIFT key while selecting multiple lines, </br>" +
-            "then click this button. To select a series of connected lines in</br>" +
-            "one step - hold CTRL while clicking on one of the lines.";
+        this.tips['group'] = "Group (Ctrl +G) </br> " +
+                            "Combine selected lines into one group to allow selection in </br> " +
+                            "one click. Hold the SHIFT key while selecting multiple lines, </br>" +
+                            "then click this button. To select a series of connected lines in</br>" +
+                            "one step - hold CTRL while clicking on one of the lines.";
         this.tips['ungroup'] = "Ungroup (Ctrl+U)</br>Separates a group into its component lines."
         this.tips['fitScreen'] = "Zoom to fit screen (=)</br>Set magnification to show the full design.";
         this.tips['actualSize'] = "Zoom actual size (1)</br>Show design approximately in real-life size.";
-        this.tips['zoomTool'] = "Zoom</br>To modify a specific area, in workspace drag a rectangle</br> around the area.</br>" +
-            "To set center of magnification and increase magnification,</br> click at desired new center of drawing.</br>" +
-            "To set center of magnification and decrease magnification,</br> right click at desired new center of drawing.";
-        this.tips['delete'] = "Delete (Del)</br>Remover part of your drawing. Click the selection (arrow)</br>" +
+        this.tips['zoomTool'] = "Zoom</br>" +
+            "To modify a specific area, in workspace drag a rectangle</br> " +
+            "around the area. To set center of magnification and increase</br>" +
+            "magnification, click at desired new center of drawing.</br>" +
+            "To set center of magnification and decrease magnification,</br> " +
+            "right click at desired new center of drawing.";
+        this.tips['delete'] = "Delete (Del)</br>" +
+            "Remover part of your drawing. Click the selection (arrow)</br>" +
             "button, then click the line to remove, then click the button.";
         this.tips['preferences'] = "Preferences (F2)</br> Set preference options.";
         this.tips['toggle'] = "Toggle inch/metric mode</br> Show all dimensions on inches or in millimeters";
         this.tips['intersect'] = "Intersect (Ctrl+I)</br> Split lines where they cross.";
-        this.tips['3D'] = "3D Preview (Ctrl+R)</br> Show 3D view of the current design.</br> Use frequently during creating of your drawing.";
-        this.tips['price-analyze'] = "Price/Analyze (F9)</br> Check design validity and compute cost.</br>" +
-            "You can also use this button to guide you thru the steps. For</br>example, if you click this button with a blank " +
-            "screen you will</br> be advised to draw a line";
+        this.tips['3D'] = "3D Preview (Ctrl+R)</br> " +
+            "Show 3D view of the current design.</br> " +
+            "Use frequently during creating of your drawing.";
+        this.tips['price-analyze'] = "Price/Analyze (F9)</br>" +
+            "Check design validity and compute cost. You can also use this button </br>" +
+            " to guide you thru the steps. For example, if you click this button </br>" +
+            "with a blank screen you will be advised to draw a line";
 
-        this.tips['select-tool'] = "Select</br>Chooses a line to which you want to issue a command or make a change.</br>" +
-            " Click on the line. To select multiple lines hold the CTRL key.</br> To select connected lines hold the SHIFT key.";
-        this.tips['line-tool'] = "Line</br> Draws a straight line. Click at the start and  end points. Hold the CTRL key </br>" +
-            "while drawing for a precise 0, 15, 30 or 45 deg angle.</br> Press spacebar to restart line mode.";
-        this.tips['spline-tool'] = "Spline</br>Draws a special style of curve.</br> In workarea click to specify start point of the curve.</br>" +
-            " Click again at end point.</br> Drag the control points to modify  the curve.";
+        this.tips['select-tool'] = "Select</br>" +
+            "Chooses a line to which you want to issue a command or make a change.</br>" +
+            " Click on the line. To select multiple lines hold the CTRL key.</br> " +
+            "To select connected lines hold the SHIFT key.";
+        this.tips['line-tool'] = "Line</br> " +
+            "Draws a straight line. Click at the start and  end points. </br>" +
+            "Hold the CTRL key while drawing for a precise 0, 15, 30 or </br> " +
+            "45 deg angle. Press spacebar to restart line mode.";
+        this.tips['spline-tool'] = "Spline</br>" +
+            "Draws a special style of curve. In workarea click to specify</br> " +
+            "start point of the curve. Click again at end point.</br>" +
+            "Drag the control points to modify  the curve.";
         this.tips['rect-tool'] = "Rectangle</br>Draws a rectangle. </br>In workarea, click at opposite corners of the rectangle.";
         this.tips['circle-tool'] = "Circle</br> Draws a circle. In workarea, click at the center and circumference.";
-        this.tips['freehand-tool'] = "Freehand</br> Draws a freehand sketch.</br> Click and hold mouse button while moving the mouse.</br>" +
+        this.tips['freehand-tool'] = "Freehand</br> " +
+            "Draws a freehand sketch.</br> " +
+            "Click and hold mouse button while moving the mouse.</br>" +
             "Use the Node edit tool to modify.";
-        this.tips['eraser-tool'] = "Eraser</br> Use  this essential tool to create most shapes.</br> First draw overlapping circles, rectangles and straight lines." +
-            "</br> Then click this Eraser tool and click on the appropriate line segments to be erased.</br>" +
-            " For example, try creating a “D” shape by drawing a vertical line through a circle and </br>" +
-            "then erasing the appropriate line segments. Or try drawing a thick ”+” shape by first drawing overlapping </br>" +
-            "vertical and horizontal rectangles and then erasing the internal segments.";
-        this.tips['corner-tool'] = "Corner</br> Rounds or chamfers sharp 2D corners.</br> First select two or more lines that meet.";
-        this.tips['text-tool'] = "Text</br> Adds comment text to design.</br> In workarea, click to specify starting point. Type the text and press Enter.";
-        this.tips['line-edit-tool'] = "Line Edit</br>Allows to move line endpoints to adjust a shape.</br> In workarea click on the line.</br>" +
-            " Drag the line or its endpoint to the desired location.";
+        this.tips['eraser-tool'] = "Eraser</br> " +
+            "Use  this essential tool to create most shapes.</br> " +
+            "First draw overlapping circles, rectangles and </br>" +
+            "straight lines. Then click this Eraser tool and </br>" +
+            "click on the appropriate line segments to be erased.</br>" +
+            "For example, try creating a “D” shape by drawing </br>" +
+            "a vertical line through a circle and then erasing </br>" +
+            "the appropriate line segments. Or try drawing a thick ”+” </br>" +
+            "shape by first drawing overlapping vertical and horizontal</br>" +
+            " rectangles and then erasing the internal segments.";
+        this.tips['corner-tool'] = "Corner</br> " +
+            "Rounds or chamfers sharp 2D corners.</br> " +
+            "First select two or more lines that meet.";
+        this.tips['text-tool'] = "Text</br> " +
+            "Adds comment text to design.</br> " +
+            "In workarea, click to specify starting point." +
+            "Type the text and press Enter.";
+        this.tips['line-edit-tool'] = "Line Edit</br>" +
+            "Allows to move line endpoints to adjust a shape.</br> " +
+            "In workarea click on the line.</br>" +
+            "Drag the line or its endpoint to the desired location.";
         this.tips['ruler-tool'] = "Ruler </br> Measures a distance in the workspace.</br> Left-click on the start and end points to be measured.";
         this.tips['snap-tool'] = "Snap to lines</br> Places new or moved lines to meet key points on existing lines. </br>" +
             "It is generally recommended to keep snap mode active.";
@@ -60,14 +88,18 @@ export default class Tips{
         this.tips['numeric-vertical-size'] = "Vertical size </br> Height of imaginary rectangle enclosing the line.";
         this.tips['numeric-line-length'] = "Length</br> Distance from the beginning of the line to the end.";
         this.tips['numeric-line-angle'] = "Line angle</br> Angle of the end point with respect to the start point.";
-        this.tips['numeric-repeat'] = "Repeat (Ctrl+D)</br>When this button is pressed in, </br>" +
-            "the nudge arrow buttons and the nudge rotation buttons will create</br> copies of the selected shape.</br>" +
-            " For example, to create 3 copies to the right of an existing shape,</br>" +
-            "select the shape, press in this Repeat button and press the right arrow button or key 3 times.";
+        this.tips['numeric-repeat'] = "Repeat (Ctrl+D)</br>" +
+            "When this button is pressed in, the nudge arrow buttons and the </br>" +
+            "nudge rotation buttons will create copies of the selected shape.</br>" +
+            "For example, to create 3 copies to the right of an existing shape,</br>" +
+            "select the shape, press in this Repeat button and press the right </br>" +
+            "arrow button or key 3 times.";
         this.tips['numeric-nudge'] = "Nudge selected line the specified distance.</br> " +
             "Hold the Alt key for faster nudging.</br> The state of Repeat button determines if lines are duplicated or simply moved.";
-        this.tips['numeric-nudge-step'] = "Nudge step </br> The distance an object will move when an arrow button or key is pressed.</br>" +
-            " It is generally recommended to move an item by first dragging</br> the item to a snap point on an existing line and then nudging.";
+        this.tips['numeric-nudge-step'] = "Nudge step </br> " +
+            "The distance an object will move when an arrow button or key is pressed.</br>" +
+            " It is generally recommended to move an item by first dragging</br> " +
+            "the item to a snap point on an existing line and then nudging.";
         this.tips['numeric-rotate-counterclockwise'] = "Rotate selected line Left (counterclockwise) </br>" +
             "            The state of the Repeat button determines</br> if lines are duplicated or simply rotated.";
         this.tips['numeric-rotate-clockwise'] = "Rotate selected line Right (clockwise) </br>The state of the Repeat button determines</br>" +
